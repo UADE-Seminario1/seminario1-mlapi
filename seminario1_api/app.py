23bc24edf104d748da685bfdef4e07f4f74c7be5
Alexander Cabezas <acabezas@inmarket.com>
2021-10-15 21:50:26 -0300

    Cambio menor

--- seminario1_api/app.py
+++ seminario1_api/app.py
@@ -1,8 +1,6 @@
 import os
 from flask import Flask, jsonify
-# from flask import (
-#     Blueprint, flash, redirect, render_template, request, url_for, jsonify
-# )
+
 import tensorflow as tf
 from tensorflow import keras
 from keras.applications import xception
@@ -11,13 +9,7 @@ from keras.preprocessing import image
 
 app = Flask(__name__)
 
-# bp = Blueprint('api', __name__)
-#os.path.abspath(os.path.dirname(__name__)) + 
-
-# graph = tf.compat.v1.get_default_graph()
-# with graph.as_default():
-# model = keras.models.load_model("trained_recycled_model_h5.h5")
-
+model = keras.models.load_model("trained_recycled_model_h5.h5")
 
 
 @app.route("/")
@@ -25,7 +17,7 @@ def get_health():
     
     info = dict(
         status='OK',
-        #model=model.layers[0].input_shape
+        model=model.layers[0].input_shape
     )
 
     return jsonify(info)
